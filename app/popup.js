eaa0643535b4baa422f01a71f1cc5ca9a4989948
cidkardi <144916094+cidkardi@users.noreply.github.com>
2025-04-21 20:32:20 -0400

    Update popup.js

--- app/popup.js
+++ app/popup.js
@@ -1,7 +1,7 @@
 var video_script = null;
 
 if (typeof base_url == "undefined") {
-  var base_url = "https://raw.githubusercontent.com/ading2210/edpuzzle-answers/main";
+  var base_url = "https://raw.githubusercontent.com/cidkardi/edhack/main";
 }
 
 function http_exec(url) {
@@ -14,16 +14,12 @@ function skip_video() {
   var button = document.getElementById("skipper");
   button.disabled = true; 
   button.value = "Downloading script...";
-
   http_exec(base_url+"/app/skipper.js");
 }
 
 function answer_questions() {
-  var skipper = document.getElementById("skipper");
   var button = document.getElementById("answers_button");
-  skipper.disabled = true;
-  button.disabled = true; 
+  button.disabled = true;
   button.value = "Downloading script...";
-
   http_exec(base_url+"/app/autoanswers.js");
 }
